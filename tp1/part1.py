115a771763f9365b1356ac600edf18bd2726baee
NUI <thichthat@gmail.com>
2019-01-29 23:04:56 +0100

    Add files via upload

--- tp1/part1.py
+++ tp1/part1.py
@@ -2,9 +2,10 @@ def dec2bin(n):
     #return "{0:b}".format(n)
     result = ""
     while n > 0:
+        # divide n by 2, and take the rest
         result = str(n % 2) + result
+        # divide n by 2, take the result (integer) and save it back to n
         n = n // 2
-
     return result
 
 print(dec2bin(148))
@@ -13,10 +14,10 @@ def hexa2dec(c):
     #return int(c, 16)
     n = 0
     for (i,ch) in enumerate(c):
-        val = "0123456789ABCDEF".index(ch)
-        ind = len(c) - i - 1
-        n = n + val * (16 ** ind)
+        digit = "0123456789ABCDEF".index(ch) # get value of this digit in decimal
+        pos = len(c) - i - 1 # position of this digit
+        n = n + digit * (16 ** pos) # add the value of this digit to n
     return n
-        
+
 
 print(hexa2dec("ABCD"))
--- tp1/part2.py
+++ tp1/part2.py
@@ -1,34 +1,51 @@
 def somme2bits(bit1, bit2):
+    # XOR operation
+    # https://en.wikipedia.org/wiki/XOR_gate
     return str(int(bit1) ^ int(bit2))
 
+
 def retenue3bits(b1, b2, b3):
+    # bit shift (right) operation
+    # https://stackoverflow.com/questions/141525
     return str((int(b1) + int(b2) + int(b3)) >> 1)
 
+
 def somme(n1, n2):
-    max_ch = max(len(n1), len(n2))
+    max_ch = max(len(n1), len(n2)) # maximum number of characters
     if len(n1) < len(n2):
+        # if n1 is shorter, fill the rest with 0
         padding = max_ch - len(n1)
         n1 = "0" * padding + n1
     else:
+        # if n2 is shorter, fill the rest with 0
         padding = max_ch - len(n2)
         n2 = "0" * padding + n2
 
     reminder = "0"
     result = ""
-    for i in reversed(range(max_ch)):
-        result = str(somme2bits(reminder, somme2bits(n1[i], n2[i]))) + result
+    for i in reversed(range(max_ch)): # do in reversed order
+        result = somme2bits(reminder, somme2bits(n1[i], n2[i])) + result
         reminder = retenue3bits(reminder, n1[i], n2[i])
+
     if reminder == "1":
+        # in the end, if reminder = 1, we write it to the end of result
         result = "1" + result
+
     return result
+
 
 print(somme("1011", "111"))
 
+
 def produit(a, b):
     result = "0"
+    # we will try to do multiplication by adding
     for (i, num) in enumerate(b):
         if num == "1":
-            result = somme(result, a + "0" * (len(b) - i - 1))
+            pos = len(b) - i - 1 # position of this digit
+            # add zeros to the right of the number, and add it back to result
+            result = somme(result, a + "0" * pos)
     return result
+
 
 print(produit("1010", "101"))
